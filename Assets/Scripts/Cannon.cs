4b93eb90100a3614d45711c63d4dcfa5e8810edc
kkabdol <kkabdol@gmail.com>
2014-02-04 01:22:37 +0900

    Kill The Kraken first commit

---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannon : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{ 
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButton (0)) {
+			Vector3 point = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			FaceTo (point);
+
+			if (Input.GetMouseButtonUp (0)) {
+				Fire ();
+			}
+		}
+	}
+
+	public void FaceTo (Vector3 target)
+	{
+		Vector3 relativePos = target - transform.position;
+		Quaternion rotation = Quaternion.LookRotation (relativePos, Vector3.back);
+		transform.rotation = rotation;
+	}
+
+	public void Fire ()
+	{
+
+	}
+}
