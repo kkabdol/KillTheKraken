9ce608233aa93dda0d5ad6045b9db15ed27b570f
kkabdol <kkabdol@gmail.com>
2014-02-05 01:50:08 +0900

    Prototyping
    Sketch assets applied

---
+++ Assets/Scripts/TestBase.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestBase : MonoBehaviour
+{
+	// real ocean look and feel test	
+	public GameObject realBackground;
+	private bool isRealBackgroundTurnedOn = false;
+	public void switchRealBackground ()
+	{
+		if (isRealBackgroundTurnedOn == true) {
+			realBackground.SetActive (false);
+			isRealBackgroundTurnedOn = false;
+		} else {
+			realBackground.SetActive (true);
+			isRealBackgroundTurnedOn = true;
+		}
+	}
+	
+}
---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wave : MonoBehaviour
+{
+	public float wavelength = 1f;
+	public float time = 1f;
+	public float delay = 0.1f;
+
+	void Start ()
+	{
+		iTween.MoveBy (gameObject, 
+		               iTween.Hash ("y", wavelength, 
+		             				"easeType", "easeOutQuad", 
+		             				"loopType", "pingPong", 
+		             				"time", time, 
+		             				"delay", delay));
+	}
+}
