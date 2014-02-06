67f3659f33b066dcdaedec77bbb13bc914d3fa82
kkabdol <kkabdol@gmail.com>
2014-02-07 01:44:17 +0900

    Bomb Shooting

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -3,25 +3,39 @@ using System.Collections;
 
 public class Cannon : MonoBehaviour
 {
+	public GameObject bombPrefab;
+	public Transform gunpoint;
+	public float bombPower = 300f;
+	public float bombTorque = 50f;
+
 	// Use this for initialization
 	void Start ()
 	{ 
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if (Input.GetMouseButton (0)) {
-			Vector3 point = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-			FaceTo (point);
-
-			if (Input.GetMouseButtonUp (0)) {
-				Fire ();
-			}
+		if (isTouched ()) {
+			Fire ();
 		}
 	}
 
+	bool isTouched ()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+//			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);		
+//			RaycastHit hit;
+//			return this.collider.Raycast (ray, out hit, 100.0f);
+
+			Vector3 point = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			return this.collider2D.OverlapPoint (new Vector2 (point.x, point.y));
+		} else {
+			return false;
+		}
+	}
+	
 	public void FaceTo (Vector3 target)
 	{
 		Vector3 relativePos = target - transform.position;
@@ -31,6 +45,9 @@ public class Cannon : MonoBehaviour
 
 	public void Fire ()
 	{
-
+		Debug.Log ("Fire");
+		GameObject bomb = Instantiate (bombPrefab, gunpoint.position, Quaternion.identity) as GameObject;
+		bomb.rigidbody2D.AddForce (new Vector2 (0f, bombPower));
+		bomb.rigidbody2D.AddTorque (bombTorque);
 	}
 }
---
+++ Assets/Scripts/DestroyTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyTrigger : MonoBehaviour
+{
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		Debug.Log ("DestroyTrigger Destroyed an object");
+		Destroy (other.gameObject);
+	}
+}
